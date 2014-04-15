7a8601f160770d8826df81b1ede93b5dacaa0ee1
Zach Klabunde <zbunde@gmail.com>
2014-04-15 15:49:18 -0600

    User can register, login, and logout of app
    Signed-off-by: Keri Clowes <kerilyn.clowes@gmail.com>

---
+++ spec/lib/user_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+require 'sequel'
+
+
+describe 'User Repository' do
+before do
+  @usertable = DB[:users]
+end
+
+  it 'it can create a user' do
+    id = @usertable.insert(email: "email", password: "password",)
+    expect(@usertable.where[:id => id]).to eq({id: id, email: "email", password: "password"})
+  end
+end
